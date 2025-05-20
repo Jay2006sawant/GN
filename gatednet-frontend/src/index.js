09dd20b5615ab90c9cc28b4e696d33875ba0848d
Jay2006sawant <jay242902@gmail.com>
2025-05-20 16:00:07 +0530

    Added

---
+++ gatednet-frontend/src/index.js
@@ -0,0 +1,13 @@
+// src/index.jsx (Main entry point)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+// If you have global styles, import them here
+// import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
